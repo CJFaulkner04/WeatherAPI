ead54c9edb2148fef5b736a036ffbc9c3e728d0b
Carlos Faulkner <drcjfaulkner@gmail.com>
2023-04-21 11:07:12 -0400

    moo

---
+++ script.js
@@ -0,0 +1,39 @@
+const apiKey = '0a6595057ce869f2a5f189ce6d19157b';
+
+const form = document.querySelector('#form');
+const zipcodeInput = document.querySelector('#zipcode');
+const weatherContainer = document.querySelector('#weather');
+
+function getWeather() {
+  const zipcode = zipcodeInput.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${apiKey}&units=imperial`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const date = new Date();
+        const city = data.name;
+        const temp = data.main.temp;
+        const description = data.weather[0].description;
+        const tempHi = data.main.temp_max;
+        const tempLo = data.main.temp_min;
+  
+        const weatherHtml = `
+          <h2>${city}</h2>
+          <p>${date.toLocaleDateString()}</p>
+          <h3>${temp} &deg;F</h3>
+          <p>Condition: ${description}</p>
+          <div class="HiLo">
+            <p class="high">Hi: ${tempHi} &deg;F</p>
+            <p class="low">Lo: ${tempLo} &deg;F</p>
+          </div>
+        `;
+
+      weatherContainer.innerHTML = weatherHtml;
+    })
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  getWeather();
+});
